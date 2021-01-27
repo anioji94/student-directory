78063ab01906cc9c5eca7191ed3465fac541e4ad
anioji94 <ojhaa94@gmail.com>
2021-01-27 18:28:27 +0000

    Adds user input functionality

--- directory.rb
+++ directory.rb
@@ -13,6 +13,24 @@ students = [
     ]
 # first we print the list of students
 
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    # create an empty array
+    students = []
+    # get first name
+    name = gets.chomp
+    # while the name is not empty, repeat this code
+    while !name.empty? do 
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        # get another name from the user
+        name = gets.chomp
+    end
+    # return array of students
+    students 
+end
+
 def print_header
     puts "The students of Villains Academy"
     puts "--------------"
@@ -30,6 +48,7 @@ def print_footer(names)
 end
 
 # nothing happens until we call all the methods
+students = input_students 
 print_header
 print(students)
 print_footer(students)
