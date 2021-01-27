aeac5d90c63477c336b758ed14c33ca3a77494bb
anioji94 <ojhaa94@gmail.com>
2021-01-27 18:29:32 +0000

    removed redundant array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,3 @@
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-    ]
 # first we print the list of students
 
 def input_students
