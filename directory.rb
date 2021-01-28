520125e30c8fa0d85fabd8bb33403322c4618558
anioji94 <ojhaa94@gmail.com>
2021-01-28 20:20:48 +0000

    adds interactive menu

--- directory.rb
+++ directory.rb
@@ -1,21 +1,38 @@
 # first we print the list of students
 
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    selection = gets.chomp
+        case selection
+        when "1"
+          input_students
+        when "2"
+            show_students
+        when "9"
+          exit # this will cause the program to terminate
+        else
+          puts "I don't know what you meant, try again"
+        end
+  end
+end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
-    students = []
     # get first name
     name = gets.chomp
-    # while the name is not empty, repeat this code
+    # type "end" to close the list.
     while !name.empty? do 
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
+        @students << {name: name, cohort: :nov}
+        puts "Now we have #{@students.count} students"
         # get another name from the user
+        puts "Would you like to add more?"
         name = gets.chomp
     end
     # return array of students
-    students 
 end
 
 def print_header
@@ -23,8 +40,8 @@ def print_header
     puts "--------------"
 end
 
-def print(students)
-   students.each_with_index do |student, num|
+def print_students_list
+   @students.each_with_index do |student, num|
         puts "#{num + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end 
 end
@@ -34,11 +51,16 @@ def print_footer(names)
     puts "Overall we have #{names.count} great students"
 end
 
-# nothing happens until we call all the methods
-students = input_students 
-print_header
-print(students)
-print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
 
+def show_students
+  print_header
+  print_students_list()
+  print_footer(@students)
+end
 
-
+interactive_menu
